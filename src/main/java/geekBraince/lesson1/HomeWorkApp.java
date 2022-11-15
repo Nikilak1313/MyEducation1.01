f2a7736042fc5619ebad7238d68137662dd39a08
Pawel <nikilak@mail.ru>
2022-11-15 12:53:00 +0700

    Препод показал как надо, исправил недочеты

--- src/main/java/geekBraince/lesson1/HomeWorkApp.java
+++ src/main/java/geekBraince/lesson1/HomeWorkApp.java
@@ -1,48 +1,51 @@
 package geekBraince.lesson1;
 
 public class HomeWorkApp {
-    public static void main(String[] args) {
-        printThreeWords();
-        checkSumSign();
-        printColor();
-        compareNumbers();
-    }
     public static void printThreeWords() {
-            System.out.println("_Orange");
-            System.out.println("_Banana");
-            System.out.println("_Apple");
-        }
-        public static void checkSumSign() {
-        int a = -10, b = 4 ;
-        if (a + b >= 0) {
+        System.out.println("_Orange");
+        System.out.println("_Banana");
+        System.out.println("_Apple");
+    }
+
+    public static void checkSumSign(int a, int b) {
+               if (a + b >= 0) {
             System.out.println("Сумма положительная");
         }
         if (a + b < 0) {
             System.out.println("Сумма отрицательная");
         }
 
-        }
-    public static void printColor() {
-        int value = 105;
-        if (value <= 0) {
+    }
+
+    public static void printColor(int value) {
+            if (value <= 0) {
             System.out.println("Красный");
         }
-        if (value > 0 && value <=100) {
+            else if (value <=100)
+        {
             System.out.println("Желтый");
         }
-        if (value > 100) {
+        else {
             System.out.println("Зеленый");
         }
 
     }
-    public static void compareNumbers() {
-        int a = 8, b = 2;
+
+    public static void compareNumbers(int a, int b) {
+
         if (a >= b) {
             System.out.println("a >= b");
-        }
-        else {
+        } else {
             System.out.println("a < b"); // считаю что в задании неточность. При а > б выводится a >= b. Возможно стоит сделать варианты а>b и a=b ?
-              }
+        }
+    }
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign(2, -4);
+        printColor(105);
+        printColor(0);
+        printColor(100);
+        compareNumbers(4, 6);
     }
 }
-
